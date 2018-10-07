ada839c8e3fc552020c2a23d597400a69db9bd3d
Jonathan Schenk <jds8899@rit.edu>
2018-10-06 20:00:42 -0400

    Adding feather_sep.py. Needs some work.

---
+++ image_proc/feather_sep.py
@@ -0,0 +1,125 @@
+import sys
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import argrelextrema
+
+HORIZONTAL_STRUC_DIV = 15
+VERTICAL_STRUC_DIV = 15
+
+def show_wait_destroy(winname, img):
+    cv.imshow(winname, img)
+    cv.moveWindow(winname, 500, 0)
+    cv.waitKey(0)
+    cv.destroyWindow(winname)
+
+def main(argv):
+    img_src = cv.imread(argv[1], cv.IMREAD_COLOR)
+    h, w, ch = img_src.shape
+    img_gs = cv.cvtColor(img_src, cv.COLOR_BGR2GRAY)
+    sig = np.zeros(w)
+
+    for i in range (0, w):
+        for j in range (0, h):
+            sig[i] += img_gs[j,i]
+
+    sig = smooth(sig, 57, 'blackman')
+    minima = argrelextrema(sig, np.greater)
+    minima = minima[0]
+    feathers = []
+    for i in range (0, len(minima) - 1):
+        feathers.append(img_src[0:h, minima[i]:minima[i+1]])
+
+    print(len(feathers))
+    for i in range (0, len(feathers)):
+        show_wait_destroy("feather", feathers[i])
+    plt.plot(sig)
+    #plt.show()
+    plt.savefig('signal.png')
+
+def smooth(x,window_len=11,window='hanning'):
+    """smooth the data using a window with requested size.
+
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+
+    input:
+        x: the input signal
+        window_len: the dimension of the smoothing window; should be an odd integer
+        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+            flat window will produce a moving average smoothing.
+
+    output:
+        the smoothed signal
+
+    example:
+
+    t=linspace(-2,2,0.1)
+    x=sin(t)+randn(len(t))*0.1
+    y=smooth(x)
+
+    see also:
+
+    numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
+    scipy.signal.lfilter
+
+    TODO: the window parameter could be the window itself if an array instead of a string
+    NOTE: length(output) != length(input), to correct this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
+    """
+
+    if x.ndim != 1:
+        raise ValueError, "smooth only accepts 1 dimension arrays."
+
+    if x.size < window_len:
+        raise ValueError, "Input vector needs to be bigger than window size."
+
+
+    if window_len<3:
+        return x
+
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+
+
+    s=np.r_[x[window_len-1:0:-1],x,x[-2:-window_len-1:-1]]
+    #print(len(s))
+    if window == 'flat': #moving average
+        w=np.ones(window_len,'d')
+    else:
+        w=eval('np.'+window+'(window_len)')
+
+    y=np.convolve(w/w.sum(),s,mode='valid')
+    return y
+
+def savitzky_golay(y, window_size, order, deriv=0, rate=1):
+    from math import factorial
+
+    try:
+        window_size = np.abs(np.int(window_size))
+        order = np.abs(np.int(order))
+    except ValueError, msg:
+        raise ValueError("window_size and order have to be of type int")
+    if window_size % 2 != 1 or window_size < 1:
+        raise TypeError("window_size size must be a positive odd number")
+    if window_size < order + 2:
+        raise TypeError("window_size is too small for the polynomials order")
+    order_range = range(order+1)
+    half_window = (window_size -1) // 2
+    # precompute coefficients
+    b = np.mat([[k**i for i in order_range] for k in range(-half_window, half_window+1)])
+    m = np.linalg.pinv(b).A[deriv] * rate**deriv * factorial(deriv)
+    # pad the signal at the extremes with
+    # values taken from the signal itself
+    firstvals = y[0] - np.abs( y[1:half_window+1][::-1] - y[0] )
+    lastvals = y[-1] + np.abs(y[-half_window-1:-1][::-1] - y[-1])
+    y = np.concatenate((firstvals, y, lastvals))
+    return np.convolve( m[::-1], y, mode='valid')
+
+if __name__ == "__main__":
+    if(len(sys.argv) < 1):
+        print("Usage: python test.py filename")
+    else:
+        main(sys.argv)
